d3d1d8f67d79112638cb6a49e89df6f8b6f99df2
jeffrey-fong <jeffreyfong94@gmail.com>
2024-01-18 09:56:25 +0800

    wip

--- hotel_planning/main.py
+++ hotel_planning/main.py
@@ -1,18 +1,29 @@
 import os
+from typing import Dict, List
 
 import requests
+from openai import OpenAI
+
+
+def infer_model(messages: List[Dict], model: str):
+    output = openai_client.chat.completions.create(
+        model=model, messages=messages, tools=tools, temperature=0.0
+    )
 
 
 def call_hotels_com_provider_api(api_name: str, input_data: dict, rapidapi_key: str):
     urls = {
-        "hotels_search": "https://hotels-com-provider.p.rapidapi.com/v2/hotels/search"
+        "hotel_details": "https://hotels-com-provider.p.rapidapi.com/v2/hotels/details",
+        "hotel_info": "https://hotels-com-provider.p.rapidapi.com/v2/hotels/info",
+        "regions_search": "https://hotels-com-provider.p.rapidapi.com/v2/regions",
+        "hotels_search": "https://hotels-com-provider.p.rapidapi.com/v2/hotels/search",
     }
 
     if urls.get(api_name):
         url = urls[api_name]
     else:
         print(f"Invalid API Name: `{api_name}`")
-        return
+        return None
 
     headers = {
         "X-RapidAPI-Key": rapidapi_key,
@@ -25,9 +36,11 @@ def call_hotels_com_provider_api(api_name: str, input_data: dict, rapidapi_key: 
         print(
             f"API call error: code-> {response.status_code} | message: {response.text}"
         )
-        return
+        return None
 
     results = response.json()
+
+    breakpoint()
 
     if api_name == "hotels_search":
         results = results["properties"]
@@ -39,11 +52,24 @@ def call_hotels_com_provider_api(api_name: str, input_data: dict, rapidapi_key: 
                     "name": result["name"],
                 }
             )
-        breakpoint()
+        return final_res
+    elif api_name == "regions_search":
+        region_id = results["data"][0]["gaiaId"]
+        return {"region_id": region_id}
 
 
 if __name__ == "__main__":
     rapidapi_key = os.environ["RAPIDAPI_KEY"]
+    openai_client = OpenAI(base_url="http://localhost:8000/v1", api_key="functionary")
+
+    query = "What hotels in Singapore are available today and checking out tomorrow?"
+
+    input_data = {"query": "Singapore", "domain": "US", "locale": "en_US"}
+
+    response = call_hotels_com_provider_api(
+        api_name="regions_search", input_data=input_data, rapidapi_key=rapidapi_key
+    )
+
     input_data = {
         "region_id": "2872",
         "locale": "en_GB",
